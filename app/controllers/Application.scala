e3ce463703bb102e1ff3c0c637d0cb97a2b85444
bleezmo <joshuarogers10@gmail.com>
2014-06-21 21:48:27 -0400

    added links

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,19 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index())
   }
-  
+  def contact = Action {
+    Ok(views.html.contact())
+  }
+  def about = Action {
+    Ok(views.html.about())
+  }
+  def faqs = Action {
+    Ok(views.html.faqs())
+  }
+  def howitworks = Action {
+    Ok(views.html.howitworks())
+  }
+  def samples = Action {
+    Ok(views.html.samples())
+  }
 }
